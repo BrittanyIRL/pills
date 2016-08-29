e69ffe4b86ca8948b886a40501920d06d3e60635
Brittany Brassell <brittany.brassell@gmail.com>
2016-08-28 19:33:57 -0700

    swapping out bootstrap for materialize to use their sidenav

---
+++ website/javascript/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	console.log("heyyyy guys");
+	 $('.button-collapse').sideNav({
+      menuWidth: 200, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+})
