40f29a39357fbe65505744460bb678fc16395c76
Brittany Brassell <brittany.brassell@gmail.com>
2016-08-29 22:58:49 -0700

    adding about and homepage, js transitions, keyframe animations, some text, filler gifs because i can, navigation, styles and overrides

--- website/javascript/script.js
+++ website/javascript/script.js
@@ -6,4 +6,20 @@ $(document).ready(function(){
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
+//cheating and using a transition jquery plugin 
+var $body    = $('html, body'); // Define jQuery collection 
+var content  = $('#main').smoothState({
+    onStart : {
+      duration: 250,
+      render: function () {
+        content.toggleAnimationClass('is-exiting');
+        
+        // Scroll user to the top
+        $body.animate({ 'scrollTop': 0 });
+
+      }
+    }
+  }).data('smoothState');
+
+//smoothState = $('#main').smoothState(options).data('smoothState');
 })
